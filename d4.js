5a08663b9529b750ac41eab95fe9194e9f0da3cb
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-04-13 10:47:16 +0200

    update

--- d4.js
+++ d4.js
@@ -85,7 +85,6 @@ let name2 = "John";
 console.log(name1 === name2);
 console.log(name1.toLowerCase() === name2.toLowerCase());
 
-
 /* WHEN YOU ARE FINISHED
  Upload the .js file on Eduflow before 5PM CET. 
 */
